56093b36e9f465df2dfe27ebb47d4fc3ce5ce60f
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-08-02 10:09:30 +0200

    added base article

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+import { ArticlesService } from './articles/articles.service';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+
+      useFactory: () => databaseConfig,
+    }),
+    ArticlesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ArticlesService],
 })
 export class AppModule {}
---
+++ src/articles/articles.entity/articles.entity.spec.ts
@@ -0,0 +1,7 @@
+import { ArticlesEntity } from './articles.entity';
+
+describe('ArticlesEntity', () => {
+  it('should be defined', () => {
+    expect(new ArticlesEntity()).toBeDefined();
+  });
+});
